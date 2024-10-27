d35836319de53193d059204a51b3e12887c8e390
Michael Gift <simiyugift@outlook.com>
2024-10-28 01:17:40 +0300

    Add Gate script for level scene

---
+++ Top-Down-RPG/Assets/Scripts/Level/Gate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField] private int levelToLoad;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>())
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+
+}
